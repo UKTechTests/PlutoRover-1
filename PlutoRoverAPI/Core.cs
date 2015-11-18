05cef2e3740ea1c041adcd78cd4836c971cdbd06
Vache Chek <vache.chek@live.co.uk>
2015-11-18 15:55:52 +0000

    added moving forward and backward

---
+++ PlutoRoverAPI/Core.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlutoRoverAPI
+{
+
+    public class Core
+    {
+
+        public Core()
+        {
+            _roverX = 0;
+            _roverY = 0;
+            _roverDirection = DirectionEnum.N;
+        }
+
+        private const int _maxX = 100;
+
+        private const int _maxY = 100;
+
+        private enum DirectionEnum
+        {
+            N,
+            E,
+            S,
+            W
+        }
+
+        private int _roverX;
+
+        private int _roverY;
+
+        private DirectionEnum _roverDirection;
+
+        public void Move(string command)
+        {
+
+            foreach (Char singleCommand in command)
+            {
+                ProcessCommand(singleCommand);
+            }
+
+        }
+
+        private void ProcessCommand(Char singleCommand)
+        {
+            switch (singleCommand)
+            {
+                case 'F':
+                    {
+
+                        if (_roverDirection == DirectionEnum.N)
+                            _roverY++;
+
+                        if (_roverDirection == DirectionEnum.S)
+                            _roverY--;
+
+                        if (_roverDirection == DirectionEnum.E)
+                            _roverX++;
+
+                        if (_roverDirection == DirectionEnum.W)
+                            _roverX--;
+
+                        break;
+                    }
+                case 'B':
+                    {
+
+                        if (_roverDirection == DirectionEnum.N)
+                            _roverY--;
+
+                        if (_roverDirection == DirectionEnum.S)
+                            _roverY++;
+
+                        if (_roverDirection == DirectionEnum.E)
+                            _roverX--;
+
+                        if (_roverDirection == DirectionEnum.W)
+                            _roverX++;
+
+                        break;
+                    }
+            }
+        }
+
+        public string GetPosition()
+        {
+            
+            return string.Format("{0}, {1}, {2}",
+                                 _roverX,
+                                 _roverY,
+                                 _roverDirection);
+
+        }
+
+    }
+
+}
--- PlutoRoverUnitTest/MovementTest.cs
+++ PlutoRoverUnitTest/MovementTest.cs
@@ -6,10 +6,64 @@ namespace PlutoRoverUnitTest
     [TestClass]
     public class MovementTest
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
 
+        #region Basic Movement
+
+        [TestMethod]
+        public void TestMoveForward2()
+        {
+            PlutoRoverAPI.Core rover = new PlutoRoverAPI.Core();
+
+            rover.Move("FF");
+
+            Assert.AreEqual("0, 2, N", rover.GetPosition());
         }
+
+        [TestMethod]
+        public void TestMoveForward2TurnRightForward2()
+        {
+            PlutoRoverAPI.Core rover = new PlutoRoverAPI.Core();
+
+            rover.Move("FFRFF");
+
+            Assert.AreEqual("2, 2, E", rover.GetPosition());
+        }
+
+        [TestMethod]
+        public void TestMoveForward3Backward2()
+        {
+            PlutoRoverAPI.Core rover = new PlutoRoverAPI.Core();
+
+            rover.Move("FFFBB");
+
+            Assert.AreEqual("0, 1, N", rover.GetPosition());
+        }
+
+        #endregion
+
+        #region Wrapping
+
+        [TestMethod]
+        public void TestMoveBackward3()
+        {
+            PlutoRoverAPI.Core rover = new PlutoRoverAPI.Core();
+
+            rover.Move("BBB");
+
+            Assert.AreEqual("0, 97, N", rover.GetPosition());
+        }
+
+        [TestMethod]
+        public void TestMoveForward2TurnLeftForward1()
+        {
+            PlutoRoverAPI.Core rover = new PlutoRoverAPI.Core();
+
+            rover.Move("BBB");
+
+            Assert.AreEqual("99, 2, W", rover.GetPosition());
+        }
+
+        #endregion
+
     }
 }
